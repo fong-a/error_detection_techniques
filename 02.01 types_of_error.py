fc8d4fbe7c8e6d49fc849055b76fa5af66985f41
Mr Fong <75341864+fong-a@users.noreply.github.com>
2022-05-11 23:58:00 -0700

    Add files via upload

---
+++ 02.01 types_of_error.py
@@ -0,0 +1,57 @@
+# This brief class will consider the different data types of error a developer can experience
+# These include:
+#       1 syntax errors
+#       2 runtime errors
+#       3 logic errors
+
+# 1 Syntax Errors
+
+# myfunction(x, y):
+#     return x + y
+
+# else:
+#     print("Hello!")
+
+# if mark >= 50
+#     print("You passed!")
+
+# if arriving:
+#     print("Hi!")
+# esle:
+#     print("Bye!")
+
+# if flag:
+# print("Flag is set!")
+
+
+# 2 Runtime Errors
+#  Some examples of runtime errors:
+
+    #   division by zero
+    #   performing an operation on incompatible types
+    #   using an identifier which has not been defined
+    #   accessing a list element, dictionary value or object attribute which doesn’t exist
+    #   trying to access a file which doesn’t exist
+
+# x = 5/0
+# print(x)
+# print("Lachlan" * "Golden")
+# print(name)
+# nums = [1,2,3,5,6,7657,4324]
+# print(nums[7])
+
+
+# 3 Logic Errors
+# Python logic errors: TryIt
+
+# width = int(input("Please enter the width of a rectangle"))
+# height = int(input("Please enter the height of a rectangle"))
+
+# area = width * height
+# print("The area of the rectangle is " + str(area))
+
+# perimeter = width + width + height
+# print("The perimeter of the rectangle is " + str(area))
+
+# if width != height:
+#   print("Your rectangle is a square")
